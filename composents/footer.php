16fd267f4ae105990d44f5cc4e987672a6977dd6
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-29 16:45:22 +0100

    ajout fichier footer.php dans dossier composents

---
+++ composents/footer.php
@@ -0,0 +1,10 @@
+
+    <hr>        
+    <footer class="footer">
+        <p><img src="../assets/img/facebook.png" alt="facebook"></p>
+        <p>Copyrigth</p>
+        <p><img src="../assets/img/instagram.png" alt="instagram"></p>
+
+    </footer>
+</body>
+</html>
