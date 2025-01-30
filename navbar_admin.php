65a8b260aff441a07e26a1f027b3baa10b75c200
sandraMasson-F <sandra.bacot@laplateforme.io>
2025-01-30 15:02:49 +0100

    ajout du module de déconnexion barre nav admin

--- navbar_admin.php
+++ navbar_admin.php
@@ -8,17 +8,23 @@
 </head>
 <body>
     <header class="header">
-        <nav>
+    <nav>
             <ul class="barre_de_nav">
-                <li><a href=""><img src="./img/accueil.png" alt="acceuil"></a></li>
+                <li><a href=""><img src="./img/accueil.png" alt="accueil"></a></li>
                 <li><a href="">Gestion des menus</a></li>
                 <li><a href="">Gestion des plats</a></li>
                 <li><a href="">Gestion des ingrédients</a></li>
-                <li><a href=""><img src="./img/administrateur.png" alt="connexion"></a></li>
+                <form method="post">
+                <button type="submit" name="deconnexion"><img src="./img/deconnexion-de-lutilisateur.png" alt="déconnexion"></button>
+                </form>
+                <?php 
+                    if (isset($_POST['deconnexion'])) {
+                        $_SESSION = array();
+                        session_destroy();
+                        header("Location:./index.php");
+                    }
+                 ?>
+            </a></li>
             </ul>
         </nav>
     </header>
-
-    <div> </div>
-</body>
-</html>
