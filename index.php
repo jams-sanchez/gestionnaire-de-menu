8660b139c1fde2c1aa720590e5b6a0c0e9ecb377
James Sanchez <james.sanchez@laplateforme.io>
2025-01-30 10:44:32 +0100

    Création de la page index, avec affichage des menus et des plats disponible

--- index.php
+++ index.php
@@ -1,63 +1,94 @@
 <?php
+
 session_start();
 
-$host = "localhost";
-$username = "root";
-$password = "";
-
 try {
-    $bdd  = new PDO("mysql:host=$host;dbname=gestion_de_menu;charset=utf8", $username, $password);
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd = new PDO('mysql:host=localhost;dbname=gestionnaire_de_menu', "root", "");
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
 }
 
-// Lorsque le bouton connexion est appuyé
-if (isset($_POST['connexion'])) {
-    // je vérifie que le champ mail et password ne sont pas vide
-    if (empty($_POST['mail']) || empty($_POST['password'])) {
-        echo "Veuillez rentrer votre adresse Mail et/ou votre mot de passe !";
-    } else {
-        // je "netoye" les paramettre donné par l'utilisateur
-        $mail = htmlentities($_POST['mail']);
-        $password = htmlentities($_POST['password']);
-        // préparation de la requête SQL
-        $sql = "SELECT * 
-        FROM utilisateur
-        WHERE mail = '$mail' AND password = '$password'";
-        $stmt = $bdd->prepare($sql);
-        $stmt->execute();
-        $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // je vérifie si l'utilisateur existe 
-        if (empty($users)) {
-            echo "Mot de passe ou identifiant incorect !";
-            // si utilisateur trouvé je redirige vers la page administrateur
-        } else {
-            $_SESSION['mail'] = $users[0]['mail'];
-            header("Location: http://localhost/gestionnaire-de-menu/pages/admin.php");
-        }
+// requete pour afficher la liste des menus
+
+$listMenuStmt = $bdd->prepare("
+SELECT menu.nom AS menu, menu.prix, plat.nom 
+FROM menu 
+JOIN plat_menu ON plat_menu.menu_id = menu.id 
+JOIN plat ON plat_menu.plat_id = plat.id
+");
+$listMenuStmt->execute();
+$listMenu = $listMenuStmt->fetchAll(PDO::FETCH_ASSOC);
+
+$menus = [];
+foreach ($listMenu as $value) {
+    $menus_nom = $value['menu'];
+    $menuPrix = $value['prix'];
+    if (!isset($menus[$menus_nom . "\n" . $menuPrix])) {
+        $menus[$menus_nom . "\n" . $menuPrix] = [];
     }
-};
+    $menus[$menus_nom . "\n" . $menuPrix][] = $value['nom'];
+}
+
+// requete pour afficher la liste des plats
+
+$listPlatStmt = $bdd->prepare("
+SELECT plat.nom, plat.description, plat.prix, plat.image, categorie.nom AS categorie 
+FROM plat 
+INNER JOIN categorie ON plat.id_categorie = categorie.id;
+");
+$listPlatStmt->execute();
+$listPlat = $listPlatStmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+<?php include './composents/navbar_user.php'; ?>
+<!-- main -->
+<main>
 
-<!DOCTYPE html>
-<html lang="en">
+    <h2 class="titre-index">Les Menus</h2>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./style.css">
-</head>
+    <section class="boite">
 
-<body>
-    <form action="index.php" method="post">
-        <input type="text" name="mail">
-        <input type="text" name="password">
-        <input type="submit" name="connexion" value="Se Connecter">
-    </form>
+        <?php foreach ($menus as $key => $value):  ?>
 
-</body>
+            <div class="card-menu">
+                <h3><?= $key . "€"; ?></h3>
+                <?php foreach ($value as $plat):  ?>
 
-</html>
+                    <p><?= $plat ?></p>
+                <?php endforeach; ?>
+
+            </div>
+
+        <?php endforeach; ?>
+
+    </section>
+
+    <h2 class="titre-index">Les plats</h2>
+
+    <section class="boite">
+
+        <?php foreach ($listPlat as $value): ?>
+
+            <div class="card-plat">
+                <img href="<?= $value['image'] ?>" />
+                <article class="card-desc">
+                    <h3><?= $value['nom'] ?></h3>
+                    <p><?= $value['description'] ?></p>
+                    <div class="prix-cat">
+                        <p><span class="bold">Prix:</span>
+                            <?= $value['prix'] ?> €</p>
+                        <p><span class="bold">Categorie:</span>
+                            <?= $value['categorie'] ?></p>
+                    </div>
+
+                </article>
+
+            </div>
+
+        <?php endforeach; ?>
+
+    </section>
+
+</main>
+<!-- footer -->
+<?php include './composents/footer.php'; ?>
