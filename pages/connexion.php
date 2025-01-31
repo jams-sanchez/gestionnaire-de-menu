44f4df559fd7c2cf7d1ae394232f510e99f82c08
Bossola-Yannick <bossolay@hotmail.fr>
2025-01-31 09:18:13 +0100

    update link and css

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $bdd = new PDO(
     'mysql:host=localhost;dbname=gestionnaire_de_menu',
     'root',
@@ -31,17 +32,20 @@ if (isset($_POST['submit'])) {
 ?>
 
 <?php include '../composents/navbar_admin.php'; ?>
-
-<h1 class="titre">Connexion</h1>
-
-<section class="bloc">
-    <form method="post" action="" class="form">
-        <input class="input" type="email" name="email" id="email" value="" placeholder="Entrez votre email" required><br /><br />
-        <input class="input" type="password" name="password" id="password" value="" placeholder="Entrez votre mot de passe" required><br /><br />
-        <button type="submit" name="submit" class="bouton">Valider</button>
-    </form>
-</section>
-
+<!-- si une session est déjà ouverte on ne propose pas de se reconnecter -->
+<?php if (isset($_SESSION['user'])) : ?>
+    <?php header("location:gestionPlat.php"); ?>
+    <!-- si pas de session ouverte on propose de se connecter -->
+<?php else : ?>
+    <h1 class="titre">Connexion</h1>
+    <section class="bloc">
+        <form method="post" action="" class="form">
+            <input class="input" type="email" name="email" id="email" value="" placeholder="Entrez votre email" required><br /><br />
+            <input class="input" type="password" name="password" id="password" value="" placeholder="Entrez votre mot de passe" required><br /><br />
+            <button type="submit" name="submit" class="bouton">Valider</button>
+        </form>
+    </section>
+<?php endif ?>
 <?php
 include '../composents/footer_admin.php';
 ?>
